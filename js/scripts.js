0af43db5e400b0578eb5fe6bb2be65167222b871
arnollder <arnoldermorozov@gmail.ru>
2025-11-20 12:39:25 +0300

    fix: adaptive доработал открытие/закрытие меню по кнопке

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-document.querySelector(".menu__button").addEventListener("click", function() {
-    document.querySelector(".menu").classList.toggle("header__menu_hidden");
-})
+// document.querySelector(".menu__button").addEventListener("click", function() {
+//     document.querySelector(".menu").classList.toggle("header__menu_hidden");
+// })
+
+function openElement() {
+    document.getElementById('header__menu_hidden').style.display = 'block';
+}
+
+function hideElement() {
+    document.getElementById('header__menu_hidden').style.display = 'none';
+}
