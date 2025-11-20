9d997795f91d2604b4ad808385a4858574a3ee71
arnollder <arnoldermorozov@gmail.ru>
2025-11-20 14:57:33 +0300

    fix: adaptive доработал функцию openAndCloseElement()

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,6 @@
 //     document.querySelector(".menu").classList.toggle("header__menu_hidden");
 // })
 
-function openElement() {
-    document.getElementById('header__menu_hidden').style.display = 'block';
-}
-
-function hideElement() {
-    document.getElementById('header__menu_hidden').style.display = 'none';
+function openAndCloseElement() {
+    document.getElementById('header__menu_hidden').classList.toggle("header__menu_hidden");
 }
