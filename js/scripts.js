b6948e66aa7273f81693961908b80cce84490a93
arnollder <arnoldermorozov@gmail.ru>
2025-11-28 10:43:53 +0300

    refactor: изменил скрипт кнопки

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
-// document.querySelector(".menu__button").addEventListener("click", function() {
-//     document.querySelector(".menu").classList.toggle("header__menu_hidden");
-// })
+document.querySelector(".menu__button").addEventListener("click", function() {
+    document.querySelector(".menu").classList.toggle("header__menu_hidden");
+})
 
-function openAndCloseElement() {
-    document.getElementById('header__menu_hidden').classList.toggle("header__menu_hidden");
-}
+document.querySelector(".header__menu_close-btn").addEventListener("click", function() {
+    document.querySelector(".menu").classList.toggle("header__menu_hidden");
+})
+
+document.querySelector(".header__menu_li").addEventListener("click", function() {
+    document.querySelector(".menu").classList.toggle("header__menu_hidden");
+})
+// function openAndCloseElement() {
+//     document.getElementById('header__menu_hidden').classList.toggle("header__menu_hidden");
+// }
+пше 
